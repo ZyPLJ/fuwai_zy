82271e51d07bae334ecdf1cb7487632bb4449b64
ZyPLJ <1767992919@qq.com>
2025-08-27 23:28:39 +0800

    fix: friend-links.ts代码格式问题修复

--- src/data/friend-links.ts
+++ src/data/friend-links.ts
@@ -147,7 +147,8 @@ export const friendLinkSections: FriendLinkSection[] = [
 			{
 				title: "酥米的小站",
 				url: "https://www.sumi233.top/",
-				avatar: "https://cdn.sumi233.top/gh/huang233893/blog-image-bed/top/huang233893/imgs/blog/userfb6a1018b84ce485.jpg",
+				avatar:
+					"https://cdn.sumi233.top/gh/huang233893/blog-image-bed/top/huang233893/imgs/blog/userfb6a1018b84ce485.jpg",
 				desc: "终有一日，寻梦中人",
 			},
 		],
